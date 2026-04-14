5df75c5c6f5b2e5a34c1d23762e712cbc8bec912
Caenis1 <carlossocial1479@gmail.com>
2026-04-14 17:33:42 -0300

    commit final

--- src/main/java/atividade_mod40/SpringBootPrimeiroExemplo/SpringBootPrimeiroExemploApplication.java
+++ src/main/java/atividade_mod40/SpringBootPrimeiroExemplo/SpringBootPrimeiroExemploApplication.java
@@ -1,5 +1,51 @@
 package atividade_mod40.SpringBootPrimeiroExemplo;
 
-public class SpringBootPrimeiroExemploApplication {
+import java.util.logging.Logger;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import atividade_mod40.domain.Cliente;
+
+@SpringBootApplication
+@Configuration
+@EnableJpaRepositories(basePackages = "br.com.rpires.repository")
+@EntityScan("br.com.rpires.*")  
+@ComponentScan(basePackages = "br.com.rpires")
+public class SpringBootPrimeiroExemploApplication implements CommandLineRunner {
+
+	private static final Logger log = LoggerFactory.getLogger(SpringBootPrimeiroExemploApplication.class);
+	
+	@Autowired
+	private IClienteRepository repository;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPrimeiroExemploApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("StartApplication...");
+		Cliente cliente = createCliente();
+		repository.save(cliente);
+	}
+	
+	private Cliente createCliente() {
+		return Cliente.builder()
+				.cidade("SP")
+				.cpf(12312312310L)
+				.email("Teste@teste.com")
+				.end("End")
+				.estado("SP")
+				.nome("Teste Spring Boot")
+				.numero(102030)
+				.tel(10203040L)
+				.build();
+	}
 
 }
--- src/main/java/atividade_mod40/repository/IClienteRepository.java
+++ src/main/java/atividade_mod40/repository/IClienteRepository.java
@@ -1,5 +1,17 @@
 package atividade_mod40.repository;
 
-public interface IClienteRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import atividade_mod40.domain.Cliente;
+
+/**
+ * @author rodrigo.pires
+ *
+ */
+@Repository
+public interface IClienteRepository extends CrudRepository<Cliente, Long>{
 
 }
+
+
